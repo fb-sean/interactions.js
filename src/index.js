d0107a13eb99876b89d09a8a36d991a44005fc07
Dominik K <dominik@koch-bautechnik.de>
2022-12-11 16:24:08 +0100

    Added version to exports

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ exports.Application = require("./application/base.js");
 exports.Embed = require("./structures/Embed.js");
 exports.ChannelManager = require("./structures/ChannelManager.js");
 exports.Colors = require("./structures/Colors.js");
+exports.version = require('../package.json').version;
